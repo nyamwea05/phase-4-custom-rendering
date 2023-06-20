49bb9990c65f82c866cdd92d7eb47b75a0f654f4
sandra <nyamweasandra@gmail.com>
2023-06-20 08:33:57 +0300

    done

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -4,4 +4,12 @@ class Cheese < ApplicationRecord
     "#{name}: $#{price}"
   end
 
+  cheese = Cheese.find_by(id: params[:id])
+  if cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  else
+    # status: :not_found will produce a 404 status code
+    render json: { error: 'Cheese not found' }, status: :not_found
+  end
+
 end
